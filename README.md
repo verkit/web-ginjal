6d4c2e24409778820bd8abfcfe52719e2409eed3
Verdy Bangkit <verdybangkit@gmail.com>
2021-11-23 00:24:26 +0700

    Pembuatan web selesai

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
-### WEB SKRIPSI
+## WEB SKRIPSI
 ___
 Website skripsi klasifikasi penyakit ginjal
 
-Build with python (Flask)
+Build with python (Flask) - 
+[sumber referensi](https://code.visualstudio.com/docs/python/tutorial-flask)
+
+### Prasyarat instalasi
+- Install [ekstensi python](https://marketplace.visualstudio.com/items?itemName=ms-python.python)
+- Install python3
+
+### Membuat environment
+```
+py -3 -m venv .venv
+.venv\scripts\activate
+```
+
+Pastikan status bar di pojok kiri bawah vscode menjadi seperti ini 
+![status-bar](https://code.visualstudio.com/assets/docs/python/shared/environment-in-status-bar.png)
+
+Apabila belum silahkan cek [tutorial](https://code.visualstudio.com/docs/python/tutorial-flask#_create-a-project-environment-for-the-flask-tutorial) ini
+
+Setelah status bar sesuai jalankan
+```
+python -m pip install --upgrade pip
+python -m pip install flask
+```
+
+Kemudian jalan kan webnya
+```
+python -m flask run
+```
---
+++ app.py
@@ -0,0 +1,118 @@
+from flask import Flask, request, render_template, jsonify, redirect
+from firebase_admin import credentials, firestore, initialize_app
+import pandas as pd
+from datetime import datetime
+import pickle
+
+# Flask App
+app = Flask(__name__)
+
+# Memuat data model yang sudah simpan
+model = pickle.load(open('gnb.pkl', 'rb'))
+
+# Inisialisasi database firestore
+cred = credentials.Certificate('key.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+history_ref = db.collection('histories')
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/history", methods=['GET'])
+def history():
+    """
+        read() : Fetches documents from Firestore collection as JSON
+        todo : Return document that matches query ID
+        all_todos : Return all documents
+    """
+    try:
+        histories = [doc.to_dict() for doc in history_ref.stream()]
+        histories.sort(key=lambda item:item['created_at'], reverse=True)
+        return render_template('history.html', histories=histories)
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+    
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    data =  {   
+        'hemo': [request.form['hemo']],
+        'al': [request.form['al']],
+        'rbcc': [request.form['rbcc']],
+        'sc': [request.form['sc']],
+        'rbc': [request.form['rbc']],
+        'bp': [request.form['bp']],
+        'su': [request.form['su']],
+        'age': [request.form['age']],
+        'dm': [request.form['dm']], 
+        'ba': [request.form['ba']],
+        'pcc': [request.form['pcc']],
+        'pc': [request.form['pc']],
+        'sg': [request.form['sg']],
+        'sod': [request.form['sod']],
+        'bu': [request.form['bu']],
+        'pot': [request.form['pot']],
+        'cad': [request.form['cad']],
+        'htn': [request.form['htn']],
+        'ane': [request.form['ane']],
+        'bgr': [request.form['bgr']],
+    }
+
+    df = pd.DataFrame(data)
+    
+    prediction = model.predict(df)
+    if prediction == 1:
+        output = 'Gagal ginjal'
+    else :
+        output = 'Normal'
+
+    data['prediction'] = output
+    data['created_at'] = datetime.now()
+
+    """
+        create() : Add document to Firestore collection with request body
+        Ensure you pass a custom ID as part of json body in post request
+        e.g. json={'id': '1', 'title': 'Write a blog post'}
+    """
+    try:        
+        history_ref.document().set(data)
+        return redirect('/history')
+    except Exception as e:
+        return f"An Error Occured: {e}"
+    
+# @app.route('/history/update', methods=['POST', 'PUT'])
+# def update():
+#     """
+#         update() : Update document in Firestore collection with request body
+#         Ensure you pass a custom ID as part of json body in post request
+#         e.g. json={'id': '1', 'title': 'Write a blog post today'}
+#     """
+#     try:
+#         id = request.json['id']
+#         history_ref.document(id).update(request.json)
+#         return jsonify({"success": True}), 200
+#     except Exception as e:
+#         return f"An Error Occured: {e}"
+
+# @app.route('/history/delete', methods=['GET', 'DELETE'])
+# def delete():
+#     """
+#         delete() : Delete a document from Firestore collection
+#     """
+#     try:
+#         # Check for ID in URL query
+#         history_id = request.args.get('id')
+#         history_ref.document(history_id).delete()
+#         return jsonify({"success": True}), 200
+#     except Exception as e:
+#         return f"An Error Occured: {e}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# port = int(os.environ.get('PORT', 8080))
+# if __name__ == '__main__':
+#     app.run(threaded=True, host='0.0.0.0', port=port)
